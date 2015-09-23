b6ed03acafa67fd170fb907676bfb49b824a40f0
natdew <natashadewitz@gmail.com>
2015-09-23 16:02:19 -0400

    challenge 1

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -7,12 +7,64 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(50, 50, 150);
+	background(53, 53, 53);
 
 	// set a fill color
 	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+fill(0, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(10, 140, 50, 50);
+
+fill(29, -0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(73, 140, 50, 50);
+
+fill(52, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(136, 140, 50, 50);
+
+fill(74, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(199, 140, 50, 50);
+
+
+fill(98, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(262, 140, 50, 50);
+
+
+fill(122, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(325, 140, 50, 50);
+
+
+fill(146, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(388, 140, 50, 50);
+
+
+fill(171, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(451, 140, 50, 50);
+
+
+fill(196, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(514, 140, 50, 50);
+
+
+fill(221, 0, 0);
+stroke(color(255, 255, 255));
+strokeWeight(1);
+rect(577, 140, 50, 50);
 }
