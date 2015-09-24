6aba76333cb73e7252d78766550f3225cb1e2a04
natdew <natashadewitz@gmail.com>
2015-09-24 09:46:51 -0400

    challenge 5

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,18 +1,10 @@
+
 function setup() {
-	// create a place to draw
 	createCanvas(640, 360);
-	noStroke();
-	noLoop();
 }
 
 function draw() {
-	// clear the background
-	background(150, 150, 50);
-
-	// set a fill color
+	background(0);
 	fill(255, 255, 255);
-
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	ellipse(mouseX, mouseY, 50, 50);
 }
